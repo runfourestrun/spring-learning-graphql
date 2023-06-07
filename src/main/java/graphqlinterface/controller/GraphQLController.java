354fe791b12fa0f53987ae0c78152d2f5860a1f5
runfourestrun <90913666+runfourestrun@users.noreply.github.com>
2023-06-07 15:48:54 -0700

    Just adding some notes and swapping graphql schema files

--- src/main/java/graphqlinterface/controller/GraphQLController.java
+++ src/main/java/graphqlinterface/controller/GraphQLController.java
@@ -8,6 +8,14 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.function.Predicate;
+
+
+/***
+ * Almost none of the resolvers have been tested. The ones I did test didn't have great results.
+ * For future me: I was trying to understand how you could use interfaces in a GraphQL Query and how you would use behavior paramterization in a GraphQL query
+ * ..... Two totally seperate topics but my mind is all over the place
+ ***/
 @Controller
 public class GraphQLController {
 
@@ -23,6 +31,14 @@ public class GraphQLController {
     @QueryMapping
     public ManagedService getServiceByName(@Argument String name){
         return this.cloudService.getManagedServiceByName(name);
+    }
+
+
+    @QueryMapping
+    public ManagedService getManagedService(@Argument Predicate<ManagedService> managedServicePredicate){
+         var filteredServices =  cloudService.filterManagedServices(managedServicePredicate);
+         return filteredServices.isEmpty() ? null : filteredServices.get(0);
+
     }
 
 
